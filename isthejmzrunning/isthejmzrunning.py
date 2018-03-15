8aa5053c851f71054d715c1aa42ba1de8481d03a
Darren Klein <darren.a.klein@gmail.com>
2018-03-14 21:49:01 -0400

    Able to make https request to MTA

--- isthejmzrunning/isthejmzrunning.py
+++ isthejmzrunning/isthejmzrunning.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
+import requests
+import env_variables
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('ISTHEJMZRUNNING_SETTINGS', silent=True)
 
 @app.route('/')
 def index():
+    r = requests.get(f'http://datamine.mta.info/mta_esi.php?key={env_variables.MTA_API_KEY}&feed_id=36')
+    print(r.text)
     return render_template('index.html')
