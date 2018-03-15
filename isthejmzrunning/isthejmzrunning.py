06b89d470e67c96db80d14b6fdbc70f8f27ed2e9
Darren Klein <darren.a.klein@gmail.com>
2018-03-14 23:18:53 -0400

    Installed gtfs-realtime-bindings, able to parse MTA data

--- isthejmzrunning/isthejmzrunning.py
+++ isthejmzrunning/isthejmzrunning.py
@@ -1,15 +1,26 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-import requests
+from google.transit import gtfs_realtime_pb2
+import urllib.request
 import env_variables
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('ISTHEJMZRUNNING_SETTINGS', silent=True)
 
+bdfm_feed_id = '21'
+jz_feed_id = '36'
+
 @app.route('/')
 def index():
-    r = requests.get(f'http://datamine.mta.info/mta_esi.php?key={env_variables.MTA_API_KEY}&feed_id=36')
-    print(r.text)
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = urllib.request.urlopen(f'http://datamine.mta.info/mta_esi.php?key={env_variables.MTA_API_KEY}&feed_id={jz_feed_id}')
+    feed.ParseFromString(response.read())
+
+    print(feed)
+
+    # for entity in feed.entity:
+    #     print(entity)
+
     return render_template('index.html')
