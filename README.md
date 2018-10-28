96e5dbdeafb70a6abd32f36e7bb22d07577370ba
Darren Klein <darren.a.klein@gmail.com>
2018-10-28 18:34:00 -0400

    Linted JS, update README.

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
 # isthejmzrunning
 
-What's up with the J/M/Z trains? This app will tell you right quick.
+In tribute to the beloved https://isthelrunning.com/, here's an app designed to give you a quick update on the general status of the J, M, and Z trains.
 
 ## Start the app
 
-``` sh
+To run with flask:
+
+```bash
 export PRODUCTION=False DEV_CONFIG=./dev_config.cfg
 flask run
 ```
 
-Locally, this app runs on `localhost:5000`.
+which will run the app on `localhost:5000`.
 
 In production, this app runs on a Gunicorn server, which can also be used locally:
 
-``` sh
+```bash
 export PRODUCTION=False DEV_CONFIG=./dev_config.cfg
 gunicorn isthejmzrunning.isthejmzrunning:app -b 0.0.0.0:8000
 ```
 
 In this example, the app would run on `localhost:8000`.
 
-## To-do
-- add logging... for now, just using print()
-
 ## Resources
+
 http://datamine.mta.info/list-of-feeds
 
 http://datamine.mta.info/sites/all/files/pdfs/GTFS-Realtime-NYC-Subway%20version%201%20dated%207%20Sep.pdf
@@ -31,4 +31,11 @@ http://datamine.mta.info/sites/all/files/pdfs/GTFS-Realtime-NYC-Subway%20version
 https://developers.google.com/transit/gtfs-realtime/
 
 ## Notes
+
 Following deployment instructions from https://medium.com/the-andela-way/deploying-your-flask-application-to-heroku-c99050bce8f9
+
+This app's JavaScript has been linted according to ESLint Airbnb base rules: https://www.npmjs.com/package/eslint-config-airbnb-base
+
+## To-do
+
+- add logging... for now, just using print()
--- isthejmzrunning/static/scripts/app.js
+++ isthejmzrunning/static/scripts/app.js
@@ -2,42 +2,42 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-$(document).ready(function() {
+$(document).ready(() => {
   const notRunningText = ['Not running. Bummer!', 'Not in service.'];
   const delayText = ['Delayed :(', 'Looks delayed. You look great!', 'Delayed. Damn!'];
   const goodText = ['All good!', 'Cool runnings!', 'Looks good. You, too!'];
   const routeContainer = $('<div>');
-  const errorContainer = $('<div>', {class: 'error_container'}).html('Sorry, there was an error. Please refresh.')
+  const errorContainer = $('<div>', { class: 'error_container' }).html('Sorry, there was an error. Please refresh.');
   const brownRouteClass = 'brown_route';
   const orangeRouteClass = 'orange_route';
 
-  $.get('/api/fetch', function(routeStatuses) {
-    routeStatuses = JSON.parse(routeStatuses);
+  $.get('/api/fetch', (routeStatuses) => {
+    const parsedRouteStatuses = JSON.parse(routeStatuses);
 
-    if (routeStatuses) {
+    if (parsedRouteStatuses) {
       $('.data_container').html(routeContainer);
 
-      for (let i = 0; i < routeStatuses.length; i++) {
-        const routeInfoContainer = $('<div>', {class: 'route_info col-xs-12 ' + (routeStatuses[i].route_id === 'M' ? orangeRouteClass : brownRouteClass)});
-        const routeIdSpan = $('<span>', {class: 'route_id_span'}).text(routeStatuses[i].route_id);
+      for (let i = 0; i < parsedRouteStatuses.length; i += 1) {
+        const routeInfoContainer = $('<div>', { class: `route_info col-xs-12 ${parsedRouteStatuses[i].route_id === 'M' ? orangeRouteClass : brownRouteClass}` });
+        const routeIdSpan = $('<span>', { class: 'route_id_span' }).text(parsedRouteStatuses[i].route_id);
         let statusText;
 
-        if (routeStatuses[i].not_running) {
+        if (parsedRouteStatuses[i].not_running) {
           statusText = notRunningText[getRandomInt(notRunningText.length)];
-        } else if (routeStatuses[i].delay_status) {
+        } else if (parsedRouteStatuses[i].delay_status) {
           statusText = delayText[getRandomInt(delayText.length)];
         } else {
           statusText = goodText[getRandomInt(goodText.length)];
         }
 
-        const statusTextSpan = $('<span>', {class: 'status_text_span'}).text(statusText);
+        const statusTextSpan = $('<span>', { class: 'status_text_span' }).text(statusText);
 
         routeInfoContainer
-          .html(routeIdSpan[0].outerHTML + ' ' + statusTextSpan[0].outerHTML)
+          .html(`${routeIdSpan[0].outerHTML} ${statusTextSpan[0].outerHTML}`)
           .appendTo(routeContainer);
-      }      
+      }
     } else {
       $('.data_container').html(errorContainer);
     }
   });
-})
+});
