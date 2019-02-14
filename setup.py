8806e8de1dcb807c261a80694363380eced8b67b
Darren Klein <darren.a.klein@gmail.com>
2019-02-14 16:49:52 -0500

    Updated flask to deal with security flaw, comment out isthejmzrunning.lib from the list of packages - was causing an error when running setup.py... this is strange, though, because that fixed things when I broke the app after adding the semver to the packages.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='isthejmzrunning',
-    packages=['isthejmzrunning, isthejmzrunning.lib'],
+    packages=['isthejmzrunning'],
+    # packages=['isthejmzrunning, isthejmzrunning.lib'],
     version='0.0.1',
     author='Darren Klein',
     description='Fetch the current status of the New York City MTA\'s J, M, and Z trains.',
     include_package_data=True,
     python_requires='>3.6.5',
     install_requires=[
-        'flask==0.12.2',
+        'flask==0.12.3',
         'gtfs-realtime-bindings==0.0.5',
         'gunicorn==19.7.1',
     ],
